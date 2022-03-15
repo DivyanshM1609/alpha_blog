eb349c33119df29e33336cb62583d23a652f1f8a
DivyanshMa <divyansh.masiwal@anchanto.com>
2022-03-15 16:47:12 +0530

    Sign up page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Defines the root path route ("/")
   # root "articles#index"
 end
