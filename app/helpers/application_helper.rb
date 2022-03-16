8b8b3fb2ede5b46ed17c3064760901b3487d776f
DivyanshMa <divyansh.masiwal@anchanto.com>
2022-03-16 16:14:27 +0530

    Add user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_for(user, size:80)
+        email_address=user.email.downcase
+        hash=Digest::MD5.hexdigest("divyanshm1609@gmail.com")
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+        image_tag(gravatar_url, alt:user.username)
+    end
 end
