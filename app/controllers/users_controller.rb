86ceb64d8d575ea5a88eae6be1de3e1a5b874f9a
DivyanshMa <divyansh.masiwal@anchanto.com>
2022-03-19 02:59:25 +0530

    Added admin access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    before_action :set_user, only:[:show, :edit, :destroy]
+    before_action :require_user, only:[:edit, :update]
+    before_action :require_same_user, only:[:edit, :update, :destroy]
+
+
     def new
         @user=User.new
     end
@@ -14,11 +19,9 @@ class UsersController < ApplicationController
 
     end
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user=User.find(params[:id])
         if(@user.update(params.require(:user).permit(:username, :email, :password)))
             flash[:notice]="User was edited successfully"
             redirect_to @user
@@ -28,16 +31,33 @@ class UsersController < ApplicationController
 
     end
     def show
-        @user=User.find(params[:id])
         @articles = @user.articles
     end
     def index
         @users=User.paginate(page:params[:page], per_page:5)
+    end
+    def destroy
+        @user.destroy
+        
+        session[:user_id]=nil if @user==current_user
+        flash[:notice]="Accounts and associated articles deleted"
+        redirect_to articles_path
     end
 
     private
     def user_params
         params.require(:user).permit(:username, :password, :email)
     end
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def require_same_user
+       
+        if current_user!=@user && !current_user.admin?
+          flash[:alert]="You can only delete or edit your own profile"
+          redirect_to users_path(@user)
+        end
+      end
 
 end
